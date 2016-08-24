e2c4b61190c1e680a9e36551c5e36d26c32b6a4a
Henrique Pin <pin.henrique03@gmail.com>
2016-08-23 22:14:42 -0300

    Criando novas estruturas na minha app

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,4 +2,17 @@ class CardsController < ApplicationController
   def show
     @card = Card.find(params[:id])
   end
+
+  def index
+    @cards = Card.all
+  end
+
+  def new
+  end
+
+  def create
+    card = Card.new(front: params[:frente], back: params[:verso])
+    card.save
+    redirect_to card
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/', to: 'home#index'
-  resources :cards, only: [:show]
+  root 'home#index'
+  #get '/', to: 'home#index', as: 'home'
+  #get '/cards/:id', to: 'cards#show' =>mesmo codigo criado com resources(linha de codigo abaixo)
+  resources :cards, only: [:show, :index, :new, :create]
 end
