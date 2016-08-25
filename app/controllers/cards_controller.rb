7d9cfbe17af5e05fc671993738578023f3dffca3
Henrique Pin <pin.henrique03@gmail.com>
2016-08-24 21:59:08 -0300

    24082016 - Atualizacao

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,6 +1,7 @@
 class CardsController < ApplicationController
+  before_action :set_card, only: [:show, :edit, :update, :destroy]
+
   def show
-    @card = Card.find(params[:id])
   end
 
   def index
@@ -8,11 +9,34 @@ class CardsController < ApplicationController
   end
 
   def new
+    @card = Card.new
   end
 
   def create
-    card = Card.new(front: params[:frente], back: params[:verso])
+    card = Card.new(card_parameters)
     card.save
     redirect_to card
+  end
+
+  def edit
+  end
+
+  def update
+    @card.update(card_parameters)
+    redirect_to @card
+  end
+
+  def destroy
+    @card.destroy
+    redirect_to root_url
+  end
+
+  private
+  def card_parameters
+    params.require(:card).permit(:front, :back)
+  end
+
+  def set_card
+    @card = Card.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'home#index'
   #get '/', to: 'home#index', as: 'home'
   #get '/cards/:id', to: 'cards#show' =>mesmo codigo criado com resources(linha de codigo abaixo)
-  resources :cards, only: [:show, :index, :new, :create]
+  #resources :cards, only: [:show, :index, :new, :create, :edit, :update]
+  resources :cards
 end
