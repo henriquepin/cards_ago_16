7a822869cb3492b2ed931c2933394a826202271a
Henrique Pin <pin.henrique03@gmail.com>
2016-08-26 14:43:07 -0300

    adicionando validacoes no model card

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,7 +1,9 @@
 class Card < ApplicationRecord
   has_many :comments
-  validates :front, presence: true
-  
+  validates :front,  presence: true
+  validates :author, presence: true
+  validates :back, presence: true
+
   def descricao_amigavel
     "PT: #{front} => EN: #{back}"
   end
