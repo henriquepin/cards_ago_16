5afbbe9b6358dd5ee39ce62e00bb8c8ef111b8ca
Henrique Pin <pin.henrique03@gmail.com>
2016-08-29 13:35:01 -0300

    add attribute to Cards

---
+++ db/migrate/20160829163244_add_published_to_cards.rb
@@ -0,0 +1,5 @@
+class AddPublishedToCards < ActiveRecord::Migration[5.0]
+  def change
+    add_column :cards, :published, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160826004545) do
+ActiveRecord::Schema.define(version: 20160829163244) do
 
   create_table "cards", force: :cascade do |t|
     t.string   "front"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20160826004545) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "author"
+    t.boolean  "published"
   end
 
   create_table "comments", force: :cascade do |t|
